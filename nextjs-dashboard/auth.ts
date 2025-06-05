161fb42a2ce67f22916b2925a1fad7fd11998237
CenterValentine <david@doxy.me>
2025-06-05 16:22:53 -0600

    W03 Test 7

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -1,15 +1,13 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { authConfig } from "./auth.config";
-import { z } from "zod";
-import type { User } from "@/app/lib/definitions";
-// import bcrypt from "bcrypt";
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
-import postgres from "postgres";
+import postgres from 'postgres';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import { authConfig } from './auth.config';
 
- 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
@@ -29,16 +27,16 @@ export const { auth, signIn, signOut } = NextAuth({
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
-        
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
+
           const user = await getUser(email);
           if (!user) return null;
+
           const passwordsMatch = await bcrypt.compare(password, user.password);
- 
           if (passwordsMatch) return user;
         }
- 
+
         console.log('Invalid credentials');
         return null;
       },
--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   experimental: {
-    ppr: 'incremental'
-  }
+  reactStrictMode: true,
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//    experimental: {
+//     ppr: 'incremental'
+//   }
+// };
+
+// export default nextConfig;
