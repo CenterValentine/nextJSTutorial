b11cdc18b2e0f7c408e574f0796c8bf0ed8e4c56
CenterValentine <david@doxy.me>
2025-06-05 21:50:20 -0600

    fix: update dependencies and fix form state handling

--- nextjs-dashboard/app/ui/login-form.tsx
+++ nextjs-dashboard/app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
@@ -8,18 +8,16 @@ import {
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 
-
-import { useActionState } from "react";
+import { useFormState } from "react-dom";
 import { authenticate } from "@/app/lib/actions";
 import { useSearchParams } from "next/navigation";
 
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined
-  );
+  const [state, formAction] = useFormState(authenticate, undefined);
+  const errorMessage = state?.error;
+
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -68,7 +66,7 @@ export default function LoginForm() {
           </div>
         </div>
         <input type="hidden" name="redirectTo" value={callbackUrl} />
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
+        <Button className="mt-4 w-full" aria-disabled={state?.isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
